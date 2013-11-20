9be8dc97375d245cdcb3ded9caa14faad3ac2799
tomy <tianyatomy@gmail.com>
2013-11-21 04:24:48 +0900

    first commit

---
+++ src/python.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="tomy"
+__date__ ="$2013-11-21 4:23:26$"
+
+if __name__ == "__main__":
+    print "Hello World"
