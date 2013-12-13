5e78f48460c2fcf581bff536eb22f6ab992e9603
tomy <tianyatomy@gmail.com>
2013-12-13 16:00:32 +0900

    python condition
    python condition

---
+++ src/condition.py
@@ -0,0 +1,13 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="tomy"
+__date__ ="$2013-11-21 4:23:26$"
+
+def a(x):
+    if x<0:
+        return -x
+    return x
+n = a(-10)
+print n
---
+++ src/python_function.py
@@ -0,0 +1,18 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="tomy"
+__date__ ="$2013-11-21 4:23:26$"
+
+def fun():
+    return 0
+
+x=int(raw_input("please input: "))
+
+if x>=90:
+    print "A"
+elif x>=80:
+    print "B"
+else:
+    print "C"
